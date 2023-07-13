334b27f1968e26987860b391839aced46d20101f
Mounir Abbad <mounirab123@gmail.com>
2023-07-13 08:38:20 -0700

    Atomic Betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 /**
-* main - A e prog
-*_
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle")
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
