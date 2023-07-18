be77c1287fb7d21b7c419b67df62c4e727aafbcf
Mounir Abbad <mounirab123@gmail.com>
2023-07-18 08:02:56 -0700

    6 Pack abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - Computes the absolute value of an integer.
+ * @n: The integer.
+ *
+ * Return: Returns to the absolute value.
+ */
+int _abs(int n)
+{
+	if (n > 0)
+		return (n);
+	else
+		return (-n);
+}
