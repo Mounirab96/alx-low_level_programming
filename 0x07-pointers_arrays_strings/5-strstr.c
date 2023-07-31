b8766397ee6733345ed80dccb3b8a7c913290d77
Mounir Abbad <mounirab123@gmail.com>
2023-07-31 08:12:42 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strstr - Is the entry point
+ * @x: input
+ * @y: input
+ * Return: Always 0 (Success)
+ */
+char *_strstr(char *x, char *y)
+{
+	for (; *x != '\0'; x++)
+	{
+		char *i = x;
+		char *j = y;
+
+		while (*i == *j && *j != '\0')
+		{
+			i++;
+			j++;
+		}
+
+		if (*j == '\0')
+			return (x);
+	}
+	return (0);
+}
