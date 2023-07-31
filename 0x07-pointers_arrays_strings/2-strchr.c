d3736fa28c42ac444605726ea677e6b49758f7d1
Mounir Abbad <mounirab123@gmail.com>
2023-07-31 07:19:31 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * _strchr - Is an entry point
- * @i: input
- * @f: input
+ * _strchr - Is the entry point
+ * @x: input
+ * @y: input
  * Return: Always 0 (Success)
  */
 
-char *_strchr(char *i, char f)
+char *_strchr(char *x, char y)
 {
 	int c = 0;
 
 	for (; x[c] >= '\0'; c++)
 	{
-		if (i[c] == f)
-			return (&i[c]);
+		if (x[c] == y)
+			return (&x[c]);
 	}
 	return (0);
 }
