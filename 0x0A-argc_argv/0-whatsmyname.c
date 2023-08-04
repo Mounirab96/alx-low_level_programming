28b1d0b2497d39fa263d96887bf89ad8e63ae923
Mounir Abbad <mounirab123@gmail.com>
2023-08-04 09:00:57 -0700

    Heisenburg

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main- a program that prints its name
+ * @argc: number of arguments passed
+ * @argv: holds the arguments passed
+ * Return: Success 0
+*/
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	int S = 0;
+
+	printf("%s\n", argv[S]);
+	return (0);
+}
