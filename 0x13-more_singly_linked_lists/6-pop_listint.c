3de20c948086b12334f01b0f426baf442ca39a40
Mounir Abbad <mounirab123@gmail.com>
2023-08-29 15:41:21 -0700

    Pop-Bob!

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node.
+ *
+ * @head: double pointer to the head.
+ *
+ * Return: the head nodes data (n).
+ */
+
+int pop_listint(listint_t **head)
+{
+	int n = 0;
+	listint_t *ptraux;
+
+	if (*head == NULL)
+		return (0);
+
+	n = (*head)->n;
+
+	ptraux = (*head)->next;
+
+	free(*head);
+
+	*head = ptraux;
+
+	return (n);
+}
