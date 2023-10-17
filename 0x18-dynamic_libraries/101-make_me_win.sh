d2d2385cca39de0262f640459262fcb2dd855436
Mounir Abbad <mounirab123@gmail.com>
2023-10-17 01:55:05 -0700

    Morocco!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,1 +1,3 @@
-wget -P https://github.com/Mounirab96/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
+#!/bin/bash
+wget -P /tmp https://github.com/Mounirab96/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD=/tmp/libgiga.so
